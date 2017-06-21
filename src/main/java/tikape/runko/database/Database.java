3b224bf02c556528b3dbe2ae53526734801662a9
Mikko Kotola <mikko.kotola@iki.fi>
2017-06-21 11:45:47 +0300

    Ja taas postgresql:n drop tablet käyttöön kun on saatu tietokanta pystyyn.

--- src/main/java/tikape/runko/database/Database.java
+++ src/main/java/tikape/runko/database/Database.java
@@ -62,9 +62,9 @@ public class Database {
         ArrayList<String> lista = new ArrayList<>();
 
         // tietokantataulujen luomiseen tarvittavat komennot suoritusjärjestyksessä
-        //lista.add("DROP TABLE Keskustelualue;");
-        //lista.add("DROP TABLE Keskustelu;");
-        //lista.add("DROP TABLE Viesti;");
+        lista.add("DROP TABLE Keskustelualue;");
+        lista.add("DROP TABLE Keskustelu;");
+        lista.add("DROP TABLE Viesti;");
         
         // heroku käyttää SERIAL-avainsanaa uuden tunnuksen automaattiseen luomiseen
         lista.add("CREATE TABLE Keskustelualue (id_keskustelualue SERIAL PRIMARY KEY, nimi_keskustelualue varchar(200));");
